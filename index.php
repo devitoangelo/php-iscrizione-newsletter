1fae3a4618f075965f5fd5d67ac11de9ea668b7f
devitoangelo <angelodevito4@gmail.com>
2024-04-16 18:56:57 +0200

    add buttom to close the alert

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
 require_once __DIR__ . '/helpers/functions.php';
-/*
-
-Descrizione
-Ciao Padawan! oggi pomeriggio ricalcate quanto visto in classe stamattina nel live.
-Andante per gradi  e seguite con ordine le milestone di seguito:
- - Milestone 1: scriviamo tutto (logica e layout) in un unico file index.php
- - Milestone 2: verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
-  Aggiungere anche un po’ di stile alla pagina tramite Bootstrap.
-
-Bonus
- - Milestone 3: invece di usare una classe statica per lo stile dell’alert,         modificarla in base all’esito della funzione. Usare alert-danger in caso di errore e alert-success in caso di esito positivo.
- - Milestone 4: invece di visualizzare il messaggio di success nella index.php, in caso di esito positivo effettuare un redirect ad una thankyou page.
- - Milestone 5: sullo stesso stile del metodo old() di Laravel, far vedere come utilizzare $_GET per valorizzare il campo di input in caso il controllo del server dia esito negativo (KO)
-
-
- */
-
-
-
 
 // var_dump($_GET);
 // var_dump(isset($_GET['email']));
@@ -87,9 +68,10 @@ if (isset($_GET["email"])) {
 
 
 
-
-
     </header>
+
+
+
 
 
     <main>
@@ -97,7 +79,9 @@ if (isset($_GET["email"])) {
         <!-- alert  -->
         <?php if (isset($message)) : ?>
 
-            <div class="alert alert-primary <?= $message['status']  ?> " role="alert">
+            <div class="alert alert-dismissible fade show  <?= $message['status']  ?> " role="alert">
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
                 <strong> <?= $message['text'] ?> </strong>
             </div>
 
